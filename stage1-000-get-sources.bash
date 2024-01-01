e2ed61fd968c7838f4c45a2795a263914398cf47
Simone Basso <bassosimone@gmail.com>
2024-01-01 11:57:45 +0100

    refactor(get sources): commit URL list, use sha256
    This diff changes how we are fetching the source files by
    using sha256 instead of md5 and by committing into the repo
    the list of URLs from which to download tarballs.

--- stage1-000-get-sources.bash
+++ stage1-000-get-sources.bash
@@ -6,14 +6,10 @@ set -euxo pipefail
 
 # create tarballs
 install -d tarballs
-
-# download the list of files to download and the MD5s in $LFS/usr/src
 cd tarballs
-curl -fsSLO https://www.linuxfromscratch.org/lfs/view/stable-systemd/wget-list-systemd
-curl -fsSLO https://www.linuxfromscratch.org/lfs/view/stable-systemd/md5sums
 
 # download all the files
-wget --input-file=wget-list-systemd --continue
+wget --input-file=../wget-list-systemd --continue
 
-# verify the MD5s
-md5sum -c md5sums
+# verify the checksum
+shasum -a 256 ../SHA256SUMS
